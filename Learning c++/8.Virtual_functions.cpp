c1ac6d677bbbcce37d4fcc43aaa607e82763e4d2
Steven <111704953+I-DoNt-LiKe-Micro-Soft@users.noreply.github.com>
2023-02-12 01:01:45 +0000

    Delete Learning c++ directory

--- Learning c++/8.Virtual_functions.cpp
+++
@@ -1,9 +0,0 @@
-/*Virtual functions
-Virtual functions allow us to overwite methods in subclasses.
-Virtual functions introduce something called dynamic dispatch which compilers typically implement via a V table which is a table containing mapping for all our virtual 
-functions except our base class. This is used so we can correctly map them to the overwitten function at runtime.
-You have to mark functions as virutal.
-
-
-MAKE AN EXAMPLE !
-*/
